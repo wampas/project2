eeacaa439dc21a612a1d98dcbf00a0df2b55a5aa
Protosac <protosacq@gmail.com>
2015-01-25 20:54:47 -0800

    Add routing specs

---
+++ spec/routing/home_routing_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe HomeController, type: :routing do
+	it 'routes to the index action' do
+		expect(get('/home/index')).to route_to('home#index')
+	end
+
+	it 'routes the root path to the index action' do
+		expect(get('/')).to route_to('home#index')
+	end
+end
