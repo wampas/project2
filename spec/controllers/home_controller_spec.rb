31b3f7066f71f442796c3dced7c78530263a5468
Protosac <protosacq@gmail.com>
2015-01-25 21:12:28 -0800

    Add controller specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
 
-  describe "#index" do
+  describe "returns 200" do
+  	before { get :index }
+
     it "returns http success" do
-      get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders home/index view" do
+    	expect(response).to render_template 'home/index'
     end
 
   end
