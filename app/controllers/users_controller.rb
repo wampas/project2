89d0873fbb2a632094aaa22cc9136a5c3f815108
Margaret Lee <Margaret.Mi.Lee@gmail.com>
2015-01-27 11:46:42 -0800

    Update to master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def create
+  		@user = User.create( user_params )
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
