ad238633eee944ebfe93e492a0ac4b5f1e684064
Protosac <protosacq@gmail.com>
2015-01-27 11:49:21 -0800

    Fix wrapper display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require query
-//= require bootstrap.min
-//= require turbolinks
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap.min
+//= require turbolinks
 //= require_tree .
