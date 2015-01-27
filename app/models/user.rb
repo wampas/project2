1fc07cddfc79f58c64b22814a68abc0315566e68
vikashiscool <vikash_parekh@yahoo.com>
2015-01-27 15:47:50 -0800

    VP Attempt at paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,11 @@ class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
+  validates_attachment :preview,
+  :content_type => { :content_type => "image/jpeg",
+  	:content_type => "image/png",
+  	:content_type => "image/gif" },
+  	:size => { :in => 0..5.megabytes }
+  
 end
