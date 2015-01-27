44d52d4a56e055da177f7a37ac325774bc637f89
vikashiscool <vikash_parekh@yahoo.com>
2015-01-27 11:07:59 -0800

    Create associations between users and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
 	validates :title, presence: true
 	validates :body, presence: true
-
+	belongs_to :user
 end
---
+++ db/migrate/20150127184816_add_name_and_avatar_to_users.rb
@@ -0,0 +1,5 @@
+class AddNameAndAvatarToUsers < ActiveRecord::Migration
+  def change
+  	add_column :users, :name, :string
+    end
+end
