9e3bfea0208f2b7f1a6ccad755ddcacf5454d9bf
vikashiscool <vikash_parekh@yahoo.com>
2015-01-27 10:17:59 -0800

    No major changes Jan 27

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,13 +1,14 @@
 module HomeHelper
+  def search(location)
+    GooglePlacesApi.query(location)
+  end
+  
   def location_name(location)
-    query(location)["formatted_address"]
+    search(location)["formatted_address"]
   end
 
   def exact_address(location)
-    query(location)["exact_address"]
+    search(location)["exact_address"]
   end
 
-  def query(location)
-    GooglePlacesApi.query(location)
-  end
 end
