55844a9e9bec02a85ddab03172322e1ec41619c1
vikashiscool <vikash_parekh@yahoo.com>
2015-01-28 11:19:08 -0800

    Begin comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
 	validates :title, presence: true
 	validates :body, presence: true
-
+	
+	belongs_to :user
+	has_many :comments, dependent: :destroy #deletes comments if article is destroyed
+	#note that we'll leave all comments, even if a user is destroyed
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :articles, dependent: :destroy  #deletes all articles if user is destroyed
+  has_many :comments #...but we'll leave all comments, even if a user is destroyed
 end
