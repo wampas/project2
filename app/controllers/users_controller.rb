b44418f23744de4cae029dd946eb69387c92ed43
Margaret Lee <Margaret.Mi.Lee@gmail.com>
2015-01-27 17:12:23 -0800

    Update on master mistake

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,4 @@
 class UsersController < ApplicationController
-	def new 
-		@user = User.new
-	end
-
-	def create
-  		@user = User.create( user_params )
-	end
 
 	def show
 		@user = User.find(params[:id])
