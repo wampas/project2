6cb073fc3c949bf59ed2e2f1e58bd3be4d3b7a50
Protosac <protosacq@gmail.com>
2015-01-29 10:08:24 -0800

    Update index, schema

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20150128222921) do
     t.datetime "image_updated_at"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
