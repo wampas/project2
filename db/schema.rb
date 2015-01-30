4ebf9d84194317bfcd6b619bae7789b4e96268e9
Margaret Lee <Margaret.Mi.Lee@gmail.com>
2015-01-29 19:00:43 -0800

    Modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20150129214905) do
     t.integer  "user_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
