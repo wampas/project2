1890ce6c77006c81a479fbfe2f0504eaa10d6231
Protosac <protosacq@gmail.com>
2015-03-02 11:26:18 -0800

    Migrate to Postgresql for Heroku deployment. Add pg gem, ran db:setup, and testested. Working on localhost.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130001236) do
+ActiveRecord::Schema.define(version: 20150202032317) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at",         null: false
@@ -35,6 +38,13 @@ ActiveRecord::Schema.define(version: 20150130001236) do
     t.text     "body"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -53,7 +63,7 @@ ActiveRecord::Schema.define(version: 20150130001236) do
     t.string   "avatar"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
