4063b415ab224103957ad5968c5f83427aba6530
Protosac <protosacq@gmail.com>
2015-03-02 21:27:20 -0800

    Updates and seed DB with articles again.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.destroy_all
-Article.destroy_all
+# User.destroy_all
+# Article.destroy_all
 
-5.times do 
-  User.create(
-    email: Faker::Internet.email,
-    password: 'password',
-    password_confirmation: 'password'
-  )
-end
+# 5.times do 
+#   User.create(
+#     email: Faker::Internet.email,
+#     password: 'password',
+#     password_confirmation: 'password'
+#   )
+# end
 
-10.times do
+5.times do
   Article.create(
   	title: Faker::Lorem.words(3).join(" ").capitalize,
-  	body: Faker::Lorem.paragraphs(3),
+  	body: Faker::Lorem.paragraphs(3).join('\n'),
     location: Faker::Address.city + ", " + Faker::Address.state_abbr,
-		user_id: Faker::Number.digit
+		user_id: Faker::Number.digit,
+    google_id: "ChIJ81iBrt--QIYRHbTlbjZLlVw"
 	)
 end
